4de23410ab963ef9d120bdcfd20072fb714e150f
Diogo Santos <diogo.jesus.tech@outlook.com>
2025-04-11 17:37:18 -0300

    feat - Remove Duplicate Number (Start Resolving)

---
+++ Arrays/26_RemoveDuplicate.js
@@ -0,0 +1,14 @@
+// Dado um array de inteiros nums ordenado em ordem não decrescente, remova as duplicatas no local de forma que cada elemento único apareça apenas uma vez. A ordem relativa dos elementos deve ser mantida. Em seguida, retorne o número de elementos únicos em nums.
+
+// Considere o número de elementos únicos de nums como k. Para ser aceito, você precisa fazer o seguinte:
+
+// Altere o array nums de forma que os primeiros k elementos de nums contenham os elementos únicos na ordem em que estavam presentes em nums inicialmente. Os elementos restantes de nums não são importantes, assim como o tamanho de nums.
+// Retorne k.
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+
+};
